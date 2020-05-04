1867a179c1a212c7141a1d71eb59a8d69704c13e
jdshepler <jshepler@umich.edu>
2020-05-04 14:42:00 -0400

    Improved UI and bug fixes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, {Component} from 'react';
 import './App.css';
+import axios from 'axios';
 import Spotify from "spotify-web-api-js";
 import TextField from '@material-ui/core/TextField';
-import axios from 'axios';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import styled from 'styled-components';
+
 
 const spotifyWebApi = new Spotify();
 
@@ -18,6 +24,7 @@ export default class App extends Component {
             loggedIn: token ? true: false,
             userID: '',
             artistName: '',
+            artistSetDate: '',
             artistSetURIs: [],
             playlistID: '',
 
@@ -57,7 +64,6 @@ export default class App extends Component {
     getUserID() {
         spotifyWebApi.getMe()
             .then(res => {
-                console.log(res)
                 this.setState({
                     userID: res.id
                 })
@@ -68,7 +74,7 @@ export default class App extends Component {
     }
 
     createSetlistPlaylist() {
-        spotifyWebApi.createPlaylist(this.state.userID, {name: "Test playlist"})
+        spotifyWebApi.createPlaylist(this.state.userID, {name: this.state.artistName + " Setlist " + this.state.artistSetDate})
             .then(res => {
                 this.setState({
                     playlistID: res.id
@@ -80,7 +86,11 @@ export default class App extends Component {
 
     addTracks() {
         spotifyWebApi.addTracksToPlaylist(this.state.playlistID, this.state.artistSetURIs)
-            .then(res => console.log(res))
+            .then(res => {
+                this.setState({
+                    artistSetURIs: []
+                })
+            })
             .catch(err => console.log(err));
     }
 
@@ -101,7 +111,6 @@ export default class App extends Component {
         }).toString();
 
         return axios.get(`${searchArtistURL}?${params}`, searchArtistConfig)
-            // .then(res => console.log(res.data))
             .then(res => {
                 this.searchSetlists(res.data.artist[0].mbid)
             })
@@ -125,48 +134,134 @@ export default class App extends Component {
         }).toString();
 
         return axios.get(`${searchSetlistsURL}?${params}`, searchSetlistsConfig)
-            // .then(res => console.log(res.data))
             .then(res => {
-                var x;
-                for (x in res.data.setlist[0].sets.set[0].song)
-                    // this.state.artistSet.push(res.data.setlist[0].sets.set[0].song[x].name);
-                    this.searchSpotifyTracks(res.data.setlist[0].sets.set[0].song[x].name, this.state.artistName)
-
-                console.log(this.state.artistSet)
+                let x;
+                let y = 0;
+                while (res.data.setlist[y].sets.set.length < 1){
+                    y++;
+                }
+                for (x in res.data.setlist[y].sets.set[0].song)
+                    this.searchSpotifyTracks(res.data.setlist[y].sets.set[0].song[x].name, this.state.artistName)
+                this.setState({
+                    artistSetDate: res.data.setlist[0].eventDate
+                })
                 this.getUserID()
             })
-
-
-            // .then(data => this.getArtistMBID(data))
             .catch(err => console.log(err));
     }
 
     render() {
+        const PaperDiv = styled.div`
+            background: linear-gradient(rgba(255,255,255, .4), rgba(255,255,255,.2));
+            border-radius: 15px;
+            margin: 30px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            padding: 25px;
+            box-shadow: 13px 15px 29px 5px rgba(0,0,0,0.25);
+        `;
+
         let home;
         if (this.state.loggedIn) {
             home = (
-                <div>
-                    <TextField
-                        id="filled-number"
+                <Grid
+                    container
+                    direction="column"
+                    justify="center"
+                    alignItems="center"
+                    spacing={2}
+                >
+                    <Grid item>
+                        <Typography
+                            variant="h2"
+                            style={{
+                            color: 'white',
+                            fontFamily: 'Quicksand'
+                        }}>
+                            SPOTLISTIFY
+                        </Typography>
+                    </Grid>
+                    <Grid item>
+                        <TextField
+                        id="outlined-number"
                         label="Enter Artist's Name"
-                        variant="filled"
+                        variant="outlined"
+                        autoFocus
                         defaultValue={this.state.artistName}
                         onChange={this.onChangeArtist}
-                    />
-                </div>
+                        style={{
+                            fontFamily: 'Quicksand'
+                        }}
+                        />
+                    </Grid>
+                    <Grid item>
+                        <Button
+                            variant="outlined"
+                            size="large"
+                            onClick={this.searchArtist}
+                            style={{
+                                color: 'black',
+                                background: 'linear-gradient(rgba(255,255,255, .9), rgba(255,255,255, .8)',
+                                borderRadius: 10,
+                                fontFamily: 'Quicksand'
+                            }}
+                        >
+                            Generate Playlist
+                        </Button>
+                    </Grid>
+                </Grid>
             )
 
         } else {
-            home = ( <a href='http://localhost:8888' > Login to Spotify </a> )
+            home = (
+                <Grid
+                    container
+                    direction="column"
+                    justify="center"
+                    alignItems="center"
+                    spacing={2}
+                >
+                    <Grid item>
+                        <Typography
+                            variant="h2"
+                            style={{
+                                color: 'white',
+                                fontFamily: 'Quicksand'
+                            }}>
+                            SPOTLISTIFY
+                        </Typography>
+                    </Grid>
+                    <Grid item>
+                        <Button
+                            variant="outlined"
+                            size="large"
+                            onClick={this.searchArtist}
+                            href='http://localhost:8888'
+                            style={{
+                                color: 'black',
+                                background: 'linear-gradient(rgba(255,255,255, .8), rgba(255,255,255, .6)',
+                                borderRadius: 5,
+                                fontFamily: 'Quicksand'
+                            }}
+                        >
+                            <h4>Login to Spotify</h4>
+                        </Button>
+                    </Grid>
+                </Grid>
+
+            )
         }
+
+
+
         return (
             <div className="App">
-                <div>
-                    { home }
-                </div>
-                <button onClick={this.searchArtist}>
-                    SearchLord
-                </button>
+                <Container maxWidth="sm">
+                    <PaperDiv>
+                        { home }
+                    </PaperDiv>
+                </Container>
             </div>
         );
     }
